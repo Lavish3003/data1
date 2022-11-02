d64be3823dfc7d15322e1566fd91dda373287304
unknown <khambalkarlavish@gmail.com>
2022-11-03 02:31:11 +0530

    Change agein filr & recover

--- code08.java
+++ code08.java
@@ -4,6 +4,7 @@ public sattic void main(String []args){
 
 Stystem.out.Println("LAVISH");
 
+<<<<<<< Updated upstream
 public void  add(){
 
 System.out.println("Add Method");
@@ -11,6 +12,14 @@ System.out.println("Add Method");
 
 }
 
+=======
+int a=10;
+int b=20;
+int c=a+b;
+System.out.println(c);
+
+
+>>>>>>> Stashed changes
 }
 
 }
